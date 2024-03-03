2792909c5ae3a1c50cad7648f79a798bcad4052e
evetsai0 <160089609+evetsai0@users.noreply.github.com>
2024-03-03 15:33:22 +0800

    1

--- DishSlide/script.js
+++ DishSlide/script.js
@@ -10,10 +10,10 @@ var TrandingSlider = new Swiper('.tranding-slider', {
     depth: 100,
     modifier: 2.5,
   },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
